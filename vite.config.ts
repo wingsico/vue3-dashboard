98fb4407aeb6a75d9c667f092cbd1fb6127fb2c7
wingsico <wingsico@gmail.com>
2020-12-08 22:11:57 +0800

    feat: use vite-babel-plugin to import element-plus on demand

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { SharedConfig } from "vite";
+import type { SharedConfig } from "vite";
+import babel from 'vite-babel-plugin';
 import path from 'path';
 
 export default {
   alias: {
     '/@/': path.resolve(__dirname, './src')
-  }
+  },
+  plugins: [
+    babel(),
+  ]
 } as SharedConfig;
