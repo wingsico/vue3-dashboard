7d6a7db88cadeb18ee25e6bf4ac7a07686f462a6
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-10 10:31:24 +0800

    fix: import full size of element, remove the useless vite-babel-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import type { SharedConfig } from "vite";
-import babel from 'vite-babel-plugin';
 import path from 'path';
 
 export default {
   alias: {
     '/@/': path.resolve(__dirname, './src')
   },
-  plugins: [
-    babel(),
-  ]
 } as SharedConfig;
